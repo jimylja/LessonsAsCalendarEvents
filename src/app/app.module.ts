52ac261995bde71e5bcfee1d5e8c9641ab9019c1
VolodimirSkripnik <volodimir.skripnik@gmail.com>
2020-02-21 08:13:30 +0200

    styling of login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,37 +14,13 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { environment } from '../environments/environment';
 
-import {
-  GoogleApiModule,
-  GoogleApiService,
-  GoogleAuthService,
-  NgGapiClientConfig,
-  NG_GAPI_CONFIG,
-  GoogleApiConfig
-} from 'ng-gapi';
+import { GoogleApiModule, NgGapiClientConfig, NG_GAPI_CONFIG } from 'ng-gapi';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const gapiClientConfig: NgGapiClientConfig = {
-  client_id: '187709823208-u52p9n3147kad6hf7pgirk52lauhmoo8.apps.googleusercontent.com',
-  discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4',
-    'https://content.googleapis.com/$ discovery/v1/apis/drive/v3/rest'],
-  ux_mode: 'popup',
-  redirect_uri: 'http://localhost:4200',
-  scope: [
-    'https://spreadsheets.google.com/feeds/spreadsheets/private/full',
-    'https://www.googleapis.com/auth/spreadsheets.readonly',
-    'https://www.googleapis.com/auth/drive',
-    'https://www.googleapis.com/auth/drive.appdata',
-    'https://www.googleapis.com/auth/drive.file',
-    'https://www.googleapis.com/auth/drive.metadata',
-    'https://www.googleapis.com/auth/drive.metadata.readonly',
-    'https://www.googleapis.com/auth/drive.photos.readonly',
-    'https://www.googleapis.com/auth/drive.readonly',
-    'https://www.googleapis.com/auth/calendar',
-    'https://www.googleapis.com/auth/calendar.events'
-  ].join(' ')
+const gApiClientConfig: NgGapiClientConfig = {
+  ...environment.gApiClient,
+  ux_mode: 'popup'
 };
-
 
 @NgModule({
   declarations: [
@@ -59,7 +35,7 @@ const gapiClientConfig: NgGapiClientConfig = {
     DashboardModule,
     GoogleApiModule.forRoot({
       provide: NG_GAPI_CONFIG,
-      useValue: gapiClientConfig
+      useValue: gApiClientConfig
     }),
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
