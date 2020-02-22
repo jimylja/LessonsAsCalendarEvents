9189b3c6d7851b813426dee20784db5207482637
VolodimirSkripnik <volodimir.skripnik@gmail.com>
2020-02-22 17:10:42 +0200

    stylization of header and footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,24 @@ import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [NotFoundComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    NotFoundComponent,
+    HeaderComponent,
+    FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
-  exports: [NotFoundComponent, HeaderComponent, FooterComponent, CommonModule]
+  exports: [
+    NotFoundComponent,
+    HeaderComponent,
+    FooterComponent,
+    CommonModule
+  ]
 })
 export class SharedModule { }
