51afdb5ae77f30e02b65b0930f5ab70be8c45866
VolodimirSkripnik <volodimir.skripnik@gmail.com>
2020-02-29 09:08:34 +0200

    realized active calendar widget

--- src/app/feature/dashboard/dashboard/active-calendar-widget/active-calendar-widget.component.ts
+++ src/app/feature/dashboard/dashboard/active-calendar-widget/active-calendar-widget.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Input } from '@angular/core';
+import { CalendarEntry } from '../../../../models/calendar-entry';
 
 @Component({
   selector: 'app-active-calendar-widget',
   templateUrl: './active-calendar-widget.component.html',
-  styleUrls: ['./active-calendar-widget.component.scss']
+  styleUrls: ['./active-calendar-widget.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ActiveCalendarWidgetComponent implements OnInit {
-
+  @Input() activeCalendar: CalendarEntry;
   constructor() { }
 
   ngOnInit() {
