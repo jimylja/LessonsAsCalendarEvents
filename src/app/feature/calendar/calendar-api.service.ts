a831ac621778a8f28ad987345a384d53af422a9e
VolodimirSkripnik <volodimir.skripnik@gmail.com>
2020-03-07 20:05:36 +0200

    realized export lessons to Google Calendar

--- src/app/feature/calendar/calendar-api.service.ts
+++ src/app/feature/calendar/calendar-api.service.ts
@@ -1,22 +1,107 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { CalendarEntry } from '../../models/calendar-entry';
-import { pluck } from 'rxjs/operators';
+import { pluck, mergeMap, delay } from 'rxjs/operators';
+import { Sheet } from '../../models/sheet';
+import * as moment from 'moment';
 
+export interface GoogleEvent {
+  summary: string;
+  description: string;
+  location: string;
+  start: {
+    dateTime: string;
+    timeZone: string;
+  };
+  end: {
+    dateTime: string;
+    timeZone: string;
+  };
+  attendees?: [{email: string}];
+  colorId?: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalendarApiService {
-  private CALENDARS_API_URL = `https://www.googleapis.com/calendar/v3/users/me/calendarList`;
+  private CALENDAR_API_URL = `https://www.googleapis.com/calendar/v3`;
+  private LESSONS_START_SCHEDULE = ['8:00', '8:55', '10:00', '11:05', '12:00', '12:55', '13:45'];
+  private LESSONS_DURATION = 45;
   constructor(private httpClient: HttpClient) {}
 
+  /**
+   * Method makes request, that takes list of user calendars
+   * @returns array, list of calendars
+   */
   getCalendars(): Observable<CalendarEntry[]> {
-    return this.httpClient.get(this.CALENDARS_API_URL).pipe(
+    return this.httpClient.get(`${this.CALENDAR_API_URL}/users/me/calendarList`).pipe(
       pluck('items')
     );
   }
 
+  /**
+   * Method creates events in Google Calendar based on spreadsheet data
+   * @param events - spreadsheet tabs, where rows represents lessons data
+   * @param calendar - calendar that will contain new events
+   */
+  exportLessonsToCalendar(events: Sheet[], calendar: CalendarEntry): void {
+    const lessons = events.map(
+      classTab => this.generateEvents(classTab, calendar.timeZone)
+    );
+    lessons.forEach(
+      classLessons => { from(classLessons).pipe(
+        mergeMap(
+          lesson => this.createEvent(lesson, calendar.id), 2
+        )
+      ).subscribe();
+      }
+    );
+  }
 
+  /**
+   * Method makes request, that creates new event in Google Calendar
+   * @returns response
+   * @param event - Google Event object
+   * @param calendarId - id of google calendar
+   */
+  private createEvent(event: GoogleEvent, calendarId: string): Observable<any> {
+    return this.httpClient.post(
+      `${this.CALENDAR_API_URL}/calendars/${calendarId}/events`,
+      event,
+      {observe: 'response'}
+    ).pipe(
+      delay(100)
+    );
+  }
+
+  /**
+   * Method generate array of event objects in Google Calendar format
+   * @returns array of of Google Calendar events
+   * @param classTab - Google Spreadsheet tab with events data
+   * @param timeZone - time zone for new events
+   */
+  private generateEvents(classTab: Sheet, timeZone: string): Array<GoogleEvent> {
+    return classTab.lessons.map(
+      lesson => {
+        const lessonDate = moment(`${lesson.date}T${this.LESSONS_START_SCHEDULE[lesson.order]}`, 'DD.MM.YYYY HH:mm');
+        return {
+          summary: `${lesson.order} урок: ${classTab.title}`,
+          location: lesson.location,
+          description: `№ ${lesson.number}: <strong>${lesson.topic}</strong>, ${lesson.hwPractice},${lesson.hwTheory}`,
+          start: {
+            dateTime: lessonDate.format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
+            timeZone
+          },
+          end: {
+            dateTime: lessonDate.clone().add(this.LESSONS_DURATION, 'minute').format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
+            timeZone
+          },
+          attendees: [{email: `${classTab.attendeesEmail}`}],
+          colorId: Number(Object.values(classTab.color)[0])
+        };
+      }
+    );
+  }
 }
--- src/app/feature/spreadsheet/spreadsheet/spreadsheet.component.ts
+++ src/app/feature/spreadsheet/spreadsheet/spreadsheet.component.ts
@@ -3,11 +3,12 @@ import { SpreadsheetService } from '../spreadsheet.service';
 import { Observable } from 'rxjs';
 import { DriveFile } from '../../../models/drive-file';
 import { CalendarEntry } from '../../../models/calendar-entry';
-import { switchMap } from 'rxjs/operators';
+import {mergeMap, switchMap} from 'rxjs/operators';
 import { Sheet } from '../../../models/sheet';
 import { select, Store } from '@ngrx/store';
 import * as fromFile from '../../file-picker/state';
 import * as fromCalendar from '../../calendar/state';
+import { CalendarApiService } from '../../calendar/calendar-api.service';
 
 @Component({
   selector: 'app-spreadsheet',
@@ -18,21 +19,31 @@ export class SpreadsheetComponent implements OnInit {
 
   activeFile$: Observable<DriveFile>;
   activeCalendar$: Observable<CalendarEntry>;
-  spreadsheetData$: Observable<Sheet[]>;
-
+  spreadSheetData: Sheet[];
+  activeCalendar: CalendarEntry;
   constructor(
     private store: Store<fromFile.State>,
-    private spreadsheetService: SpreadsheetService) { }
+    private spreadsheetService: SpreadsheetService,
+    private calendarService: CalendarApiService) { }
     displayedColumns: string[] = ['number', 'date', 'order', 'location', 'topic', 'hwTheory', 'hwPractice'];
   ngOnInit() {
     this.activeFile$  = this.store.pipe(select(fromFile.getCurrentFile));
     this.activeCalendar$ = this.store.pipe(select(fromCalendar.getCurrentCalendar));
 
-    this.spreadsheetData$ = this.activeFile$.pipe(
-      switchMap( (file: DriveFile) => {
-       return this.spreadsheetService.getSpreadsheetData(file.id);
-      }
-    ));
+    this.activeCalendar$.pipe(
+      mergeMap((calendar: CalendarEntry) => {
+        this.activeCalendar = calendar;
+        return this.activeFile$.pipe(
+          switchMap((file: DriveFile) => {
+            return this.spreadsheetService.getSpreadsheetData(file.id);
+          })
+        );
+      })
+    ).subscribe(lessons => this.spreadSheetData = lessons);
+  }
+
+  exportToCalendar() {
+    this.calendarService.exportLessonsToCalendar(this.spreadSheetData, this.activeCalendar);
   }
 
 }
