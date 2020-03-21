a40c34e16a9e2ae3a575a6273884b79bafe41cf3
VolodimirSkripnik <volodimir.skripnik@gmail.com>
2020-03-21 17:34:31 +0200

    basic test for user profile

--- src/app/feature/user/user-profile/user-profile.component.spec.ts
+++ src/app/feature/user/user-profile/user-profile.component.spec.ts
@@ -1,19 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserProfileComponent } from './user-profile.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { GoogleApiModule, GoogleApiService, GoogleAuthService, NG_GAPI_CONFIG, NgGapiClientConfig} from 'ng-gapi';
+import {environment} from '../../../../environments/environment';
 
-describe('UserProfileComponent', () => {
+export interface UserState {
+  user: {
+    isLoggedIn: boolean;
+    userProfile: any
+  };
+}
+
+fdescribe('UserProfileComponent', () => {
   let component: UserProfileComponent;
   let fixture: ComponentFixture<UserProfileComponent>;
+  let store: MockStore<UserState>;
+  const initialState = {user: {isLoggedIn: false}};
+
+  const gApiClientConfig: NgGapiClientConfig = {
+    ...environment.gApiClient,
+    ux_mode: 'popup'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserProfileComponent ]
+      declarations: [ UserProfileComponent ],
+      imports: [
+        HttpClientTestingModule,
+        GoogleApiModule.forRoot({provide: NG_GAPI_CONFIG, useValue: gApiClientConfig})
+      ],
+      providers: [GoogleAuthService, GoogleApiService, provideMockStore({initialState})],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    store = TestBed.get(Store);
     fixture = TestBed.createComponent(UserProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
