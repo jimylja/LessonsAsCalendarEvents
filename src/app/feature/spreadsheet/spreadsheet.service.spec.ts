a0eaa9e11e8101da9854b4fff41e54c32de580a8
VolodimirSkripnik <volodimir.skripnik@gmail.com>
2020-03-28 23:07:40 +0200

    was added test for spreadsheet service, impoved method that parse row data

--- src/app/feature/spreadsheet/spreadsheet.service.spec.ts
+++ src/app/feature/spreadsheet/spreadsheet.service.spec.ts
@@ -1,24 +1,46 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import { SpreadsheetService } from './spreadsheet.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatSnackBarModule} from '@angular/material';
 import { NO_ERRORS_SCHEMA} from '@angular/core';
 import { UserFacade} from '../user/user.facade';
 import { StoreModule} from '@ngrx/store';
+import { CalendarApiService} from '../calendar/calendar-api.service';
+import { dummyGridResponse, dummySheetData} from './mock/spreadsheet.mock';
+import { environment} from '../../../environments/environment';
 
 describe('SpreadsheetService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      StoreModule.forRoot({}),
-      HttpClientTestingModule, RouterTestingModule, MatSnackBarModule
-    ],
-    providers: [UserFacade],
-    schemas: [NO_ERRORS_SCHEMA]
-  }));
+  let httpMock: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({}),
+        HttpClientTestingModule, RouterTestingModule, MatSnackBarModule
+      ],
+      providers: [
+        CalendarApiService,
+        UserFacade
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
     const service: SpreadsheetService = TestBed.get(SpreadsheetService);
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch spreadsheet data from server and convert it to Sheet interface', () => {
+    const colors = ['#a4bdfc', '#7ae7bf', '#dbadff', '#ff887c'];
+    const gridId = dummyGridResponse.spreadsheetId;
+    const service: SpreadsheetService = TestBed.get(SpreadsheetService);
+    const calendarService = TestBed.get(CalendarApiService);
+    spyOn(calendarService, 'getCalendarColors').and.returnValue([colors]);
+    service.getSpreadsheetData(gridId).subscribe(grid => expect(grid).toEqual(dummySheetData));
+    const request = httpMock.expectOne(`${environment.apiEndpoints.spreadsheet}/${gridId}/?includeGridData=true`);
+    expect(request.request.method).toEqual('GET');
+    request.flush(dummyGridResponse);
   });
 });
